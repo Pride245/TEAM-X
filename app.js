420c725ed5d2258c15584392441e27e278ce7d84
Pride245 <202603777@spu.ac.za>
2026-04-06 17:12:09 +0200

    WEB PROJECT PRACTICE

---
+++ app.js
@@ -0,0 +1,15 @@
+alert("MONEY MAKES SENSE");
+alert("MONEY MAKES THE WORLD SPIN");
+alert("MAKE MONEY OR YOU GO SUFFER");
+document.body.style.background= "purple";
+
+let username = 'PRIDE';
+console.log('username');
+username = 'LESEGO';
+console.log('username');
+username = 'MODISELLE';
+console.log("username");
+
+window.alert("Are you sure you want to continue");
+window.prompt("Please enter your name");
+window.confirm("Are you sure you want to insall this?");
